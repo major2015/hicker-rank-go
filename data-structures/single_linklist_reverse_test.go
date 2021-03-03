775887b5439e14073a5ba9f0d1dd2203e818fa72
lance <lancexu.hello@gmail.com>
2021-03-03 23:59:14 +0800

    reverse single linklist

---
+++ data-structures/single_linklist_reverse_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
+
+func reverseList1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil{
+		return head
+	}
+	newHead := reverseList1(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
+
+var head = &ListNode{
+	Val: 1,
+	Next: &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 4,
+				Next: &ListNode{
+					Val: 5,
+					Next: nil,
+				},
+			},
+		},
+	},
+}
+
+func TestReverse(t *testing.T) {
+	newhead := reverseList(head)
+	assert.Equal(t, newhead.Val, 5)
+}
+
+func TestReverse1(t *testing.T) {
+	newhead := reverseList1(head)
+	assert.Equal(t, newhead.Val, 5)
+}
