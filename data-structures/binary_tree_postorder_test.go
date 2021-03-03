45c6fe6443cd645328b1233e730aab55fd496408
lance <lancexu.hello@gmail.com>
2021-03-03 21:52:13 +0800

    binary tree post order traversal

--- data-structures/binary_tree_postorder_test.go
+++ data-structures/binary_tree_postorder_test.go
@@ -14,63 +14,68 @@ import (
  *     Right *TreeNode
  * }
  */
-func inorderTraversalRecursion(root *TreeNode) []int {
+ func postorderTraversalRecursion(root *TreeNode) []int {
 	var results = []int{}
 	if root == nil {
 		return results
 	}
 
-	tmp := inorderTraversalRecursion(root.Left)
+	tmp := postorderTraversalRecursion(root.Left)
 	for _, i := range tmp {
 		results = append(results, i)
 	}
 
+	tmp = postorderTraversalRecursion(root.Right)
+	for _, i := range tmp {
+		results = append(results, i)
+	}
+	
 	results = append(results, root.Val)
-
-	tmp = inorderTraversalRecursion(root.Right)
-	for _, i := range tmp {
-		results = append(results, i)
-	}
-
 	return results
 }
 
-func inorderTraversalTraverse(root *TreeNode) []int {
+func postorderTraversalTraverse(root *TreeNode) []int {
 	var results = []int{}
 	if root == nil {
 		return results
 	}
-
+	var prev *TreeNode
 	var stack = []*TreeNode{}
+
 	for root != nil || len(stack) > 0 { // root != nil condition for first step.
 		for root != nil {
 			stack = append(stack, root)
 			root = root.Left // move to the left node of subtree
 		}
 
-		top := stack[len(stack) - 1]
-		results = append(results, top.Val)
-		root = top.Right
+		root = stack[len(stack) - 1]
 		stack = stack[:len(stack) - 1]
+		if root.Right == nil || root.Right == prev {
+			results = append(results, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stack = append(stack, root)
+			root = root.Right
+		}
 	}
 
 	return results
 }
 
-func TestInorder(t *testing.T) {
-	res := inorderTraversalRecursion(root)
-	var experts = []int{2, 4, 7, 8, 9, 10, 11, 12}
+func TestPostorder(t *testing.T) {
+	res := postorderTraversalRecursion(root)
+	var experts = []int{2, 4, 9, 8, 11, 12, 10, 7}
 	assert.Equal(t, len(res), len(experts))
 
 	for i := 0; i < len(res); i++ {
 		assert.Equal(t, res[i], experts[i])
 	}
 
-	res = inorderTraversalTraverse(root)
+	res = postorderTraversalTraverse(root)
 	assert.Equal(t, len(res), len(experts))
 
 	for i := 0; i < len(res); i++ {
 		assert.Equal(t, res[i], experts[i])
 	}
 }
-
