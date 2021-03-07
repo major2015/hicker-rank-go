af4dd22ce2b24c86b4c3e63493dbac219d067ba8
lance <lancexu.hello@gmail.com>
2021-03-07 19:36:09 +0800

    fill annotations

--- data-structures/tow_pointers_remove_duplicates_test.go
+++ data-structures/tow_pointers_remove_duplicates_test.go
@@ -10,7 +10,7 @@ func removeDuplicates(nums []int) int {
     if nums == nil {
         return 0
     }
-    i, j := 0, 0
+    i, j := 0, 0 // 双指针解题思维，同向而行，[0, i)是有效保留数据，[i, j)是已过滤无效数据，[j, len(nums))是未执行数据
     for j < len(nums) {
         if i == 0 || nums[j] != nums[i - 1] {
             nums[i] = nums[j]
